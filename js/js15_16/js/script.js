4e00de20a2b3554a860fd3ffcb87ff52b4aa230a
Begas Anna <ankka@ukr.net>
2016-06-09 00:19:02 +0300

    some fix

--- js/js15_16/js/script.js
+++ js/js15_16/js/script.js
@@ -7,13 +7,35 @@ var text = $('#formdata').val();
 console.log("text",text)
 
   $.ajax({
-             url:"https://pixabay.com/api/?key=1888689-32b6c3d1fa4ab52b9ed34d259&q='+ text +'",
+             url:"https://pixabay.com/api/?key=1888689-32b6c3d1fa4ab52b9ed34d259&q=Black+Flowers&image_type=photo",
              dataType : "jsonp",
 
-             success: function (data) {
+             success: function(data){
                console.log("data",data);
-               var pictureShow = $('#results').html(data);
-        }
-      });
+
+               var ul = document.createElement("ul");
+
+               $.each(data.hits, function(i, val){
+
+            var li = document.createElement("li");
+
+            console.log('data.hits',data.hits);
+
+           var inner = '<img class="photo" src="' + val.webformatURL + '">' +
+           '<p><a href="'+ val.pageURL+'" title="'+val.pageURL+'" target="_blank">'+val.pageURL+'</a></p>' +
+           '<p class="users">Фотограф: '+ val.user + '</p>';
+
+          li.innerHTML = inner;
+
+
+            ul.appendChild(li);
+    });
+
+    $('#results').html(ul);
+
+
+  },
+
+        });
     });
   });
