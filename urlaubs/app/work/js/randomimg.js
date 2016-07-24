9c27ad4e04d84a685dfccdd3e9c135bfb3a71535
Begas Anna <ankka@ukr.net>
2016-07-24 23:33:34 +0300

    added urlas

--- urlaubs/app/work/js/randomimg.js
+++ urlaubs/app/work/js/randomimg.js
@@ -4,19 +4,76 @@ $(function(){
     random = Math.round(random);
     return random;
 }
-var randNumber = randomWord(3,300);
+var randNumber = randomWord(3,100);
 
-
+/*sport*/
   $.ajax({
              url:'https://pixabay.com/api/?key=1888689-32b6c3d1fa4ab52b9ed34d259&q=sport&image_type=photo&per_page='+randNumber+'&orientation=horizontal&order=latest',
              dataType : "jsonp",
-
              success: function(data){
-             $.each(data.hits, function(i, val){
-
-                 document.getElementById('sport').innerHTML = '<img class="photo" src="' + val.webformatURL + ' " width=300px height=160px>';
-
+               $.each(data.hits, function(i, val){
+                 document.getElementById('sport').innerHTML = '<img class="photo" src="' + val.webformatURL + ' ">';
                  });
                 },
               });
+/*yoga*/
+  $.ajax({
+           url:'https://pixabay.com/api/?key=1888689-32b6c3d1fa4ab52b9ed34d259&q=yoga&image_type=photo&per_page='+randNumber+'&orientation=horizontal&order=latest',
+           dataType : "jsonp",
+           success: function(data){
+             $.each(data.hits, function(i, val){
+               document.getElementById('yoga').innerHTML = '<img class="photo" src="' + val.webformatURL + ' ">';
+                 });
+                },
+              });
+/*extreme*/
+  $.ajax({
+           url:'https://pixabay.com/api/?key=1888689-32b6c3d1fa4ab52b9ed34d259&q=extreme&image_type=photo&per_page='+randNumber+'&orientation=horizontal&order=latest',
+           dataType : "jsonp",
+           success: function(data){
+             $.each(data.hits, function(i, val){
+                 document.getElementById('extreme').innerHTML = '<img class="photo" src="' + val.webformatURL + ' " >';
+               });
+             },
+          });
+  /*games*/
+  $.ajax({
+           url:'https://pixabay.com/api/?key=1888689-32b6c3d1fa4ab52b9ed34d259&q=games+casino&image_type=photo&per_page='+randNumber+'&orientation=horizontal&order=latest',
+           dataType : "jsonp",
+           success: function(data){
+             $.each(data.hits, function(i, val){
+                 document.getElementById('games').innerHTML = '<img class="photo" src="' + val.webformatURL + ' " width=300px>';
+             });
+            },
+          });
+/*culture and education*/
+  $.ajax({
+           url:'https://pixabay.com/api/?key=1888689-32b6c3d1fa4ab52b9ed34d259&q=education&image_type=photo&per_page='+randNumber+'&orientation=horizontal&order=latest',
+           dataType : "jsonp",
+           success: function(data){
+             $.each(data.hits, function(i, val){
+                 document.getElementById('cultur').innerHTML = '<img class="photo" src="' + val.webformatURL + ' " width=300px>';
+             });
+            },
+         });
+/*relax*/
+  $.ajax({
+           url:'https://pixabay.com/api/?key=1888689-32b6c3d1fa4ab52b9ed34d259&q=relax+spa&image_type=photo&per_page='+randNumber+'&orientation=horizontal&order=latest',
+           dataType : "jsonp",
+           success: function(data){
+             $.each(data.hits, function(i, val){
+               document.getElementById('relax').innerHTML = '<img class="photo" src="' + val.webformatURL + ' " width=300px>';
+             });
+            },
+          });
+/*travel*/
+  $.ajax({
+           url:'https://pixabay.com/api/?key=1888689-32b6c3d1fa4ab52b9ed34d259&q=travelling&image_type=photo&per_page='+randNumber+'&orientation=horizontal&order=latest',
+           dataType : "jsonp",
+           success: function(data){
+             $.each(data.hits, function(i, val){
+              document.getElementById('travel').innerHTML = '<img class="photo" src="' + val.webformatURL + ' " width=300px>';
+             });
+           },
+        });
             });
--- urlaubs/app/work/js/script1.js
+++ urlaubs/app/work/js/script1.js
@@ -1,9 +1,60 @@
 $(document).ready(function(){
 
-  $('.grid').masonry({
-  // options
-  itemSelector: '.grid-item',
-  columnWidth: '.grid-item'
+// Инициализация Масонри, после загрузки изображений
+
+
+
+  var window_size = $(window).width();
+          if(window_size <= 768) {
+
+         $('.grid').imagesLoaded( function() {
+            $('.grid').masonry({
+              // options
+              itemSelector: '.grid-item',
+              columnWidth: '.grid-item',
+              containerStyle: { position: 'relative', width: '320px' },
+              "gutter": 10
+            });
+          });  
+          }
+
+
+          else {
+            $('.grid').imagesLoaded( function() {
+              $('.grid').masonry({
+              // options
+              itemSelector: '.grid-item',
+              columnWidth: '.grid-item',
+              containerStyle: { position: 'relative', width: '100%' },
+              "gutter": 10
+            });
+          });
+
+          }
+
+  $("#owl-demo1").owlCarousel({
+    navigation : true, // Show next and prev buttons
+    slideSpeed : 300,
+    pagination: false,
+    // paginationSpeed : 400,
+    singleItem:true
+
   });
+  $("#owl-demo2").owlCarousel({
+    navigation : true, // Show next and prev buttons
+    slideSpeed : 300,
+    pagination: false,
+    // paginationSpeed : 400,
+    singleItem:true
+
+  });
+  $("#owl-demo3").owlCarousel({
+    navigation : true, // Show next and prev buttons
+    slideSpeed : 300,
+    pagination: false,
+    // paginationSpeed : 400,
+    singleItem:true
+  });
+
 
 })
