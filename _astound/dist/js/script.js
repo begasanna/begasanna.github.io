d7f1c7f215f882307935be0c87ce7bbce51af3e6
Begas Anna <ankka@ukr.net>
2018-01-26 19:05:01 +0200

    del2

--- _astound/dist/js/script.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function(){
-
-      $('#openicon').click(function(){
-        $('#icons').slideToggle(1000);
-        $('#openicon').toggleClass('rotate_but');
-        return false;
-    });
-
-});
